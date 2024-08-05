d3ecf697f8ada6c8be9e7a487a2c4a1884f15939
thomasbolf <thomasbolf1@tamu.edu>
2024-08-04 21:16:00 -0500

    init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Discord File System
+
+This repository is a work in progress and aims to provide additional functionality to the Discord File System. Currently, it only includes a basic ping pong feature, but more features will be added soon.
+
+Stay tuned for updates!
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	token := os.Getenv("TOKEN")
+	//error check to make sure the token is provided
+	if token == "" {
+		panic("No token provided")
+	}
+	//create a new discord bot session
+	dg, err := discordgo.New("Bot " + token)
+	//being really careful here...
+	if err != nil {
+		panic("Error creating discord session")
+	}
+	dg.AddHandler(messageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	err = dg.Open()
+	if err != nil {
+		panic("Error opening connection")
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+
+}
